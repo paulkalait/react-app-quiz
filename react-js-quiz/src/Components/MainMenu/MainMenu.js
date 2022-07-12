19d27f6f3d5ef51dda02a9d7623d66101d371e14
Your Name <kalaitzidispaul@gmail.com>
2022-07-12 18:30:42 -0400

    Added bottom styling

--- react-js-quiz/src/Components/MainMenu/MainMenu.js
+++ react-js-quiz/src/Components/MainMenu/MainMenu.js
@@ -1,11 +1,15 @@
 import React, { useContext} from 'react'
 import { QuizContext } from '../../Helpers/Context'
+import './index.css'
 const MainMenu = () => {
   //destructure what we passed into the provider
   const {gameState, setGameState} = useContext(QuizContext)
   return (
+    <div className='menu-container'>
+   
     <div className='menu'>
-    <button onClick={() => setGameState('quiz')}>Start</button>
+    <button onClick={() => setGameState('quiz')} className='btn-hover color-1'>Start</button>
+    </div>
     </div>
   )
 }
