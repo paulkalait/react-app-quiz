ecc86ef0032b12825081c6873891ee90c0020015
Your Name <kalaitzidispaul@gmail.com>
2022-07-12 20:05:47 -0400

    Added restart quiz functionality

--- react-js-quiz/src/Components/End/End.js
+++ react-js-quiz/src/Components/End/End.js
@@ -4,16 +4,26 @@ import './index.css'
 import { Questions } from '../../Helpers/QuestionList'
 
 const End = () => {
-  const {gameState, setGameState, score} = useContext(QuizContext)
+  const {gameState, setGameState, score, setScore} = useContext(QuizContext)
+
+
+  const restartQuiz = () => {
+    setScore(0)
+    setGameState("menu")
+  }
+
 
 
   return (
+    <div className='endScreenContainer'>
     <div className='endScreen'><h1>Quiz Completed</h1>
     {console.log(score)}
     {score < 3 ? (
       <h3>Great Job </h3>
     ) : <h3>Not Bad. Try again?</h3>}
     <h3> <span className={score < 3 ? "bad" : 'good'}>{score}</span> / {Questions.length}</h3>
+    <button onClick={() => restartQuiz()}>Restart</button>
+    </div>
     </div>
   )
 }
