ec257540eefdf6a41d19099f58f20a15bcc7f630
Your Name <kalaitzidispaul@gmail.com>
2022-07-12 21:14:40 -0400

    added react spinning logo

--- react-js-quiz/src/Components/MainMenu/MainMenu.js
+++ react-js-quiz/src/Components/MainMenu/MainMenu.js
@@ -1,17 +1,28 @@
-import React, { useContext} from 'react'
-import { QuizContext } from '../../Helpers/Context'
-import './index.css'
+import React, { useContext } from "react";
+import { QuizContext } from "../../Helpers/Context";
+import "./index.css";
 const MainMenu = () => {
   //destructure what we passed into the provider
-  const {gameState, setGameState} = useContext(QuizContext)
+  const { gameState, setGameState } = useContext(QuizContext);
   return (
-    <div className='menu-container'>
-   
-    <div className='menu'>
-    <button onClick={() => setGameState('quiz')} className='btn-hover color-1'>Start</button>
-    </div>
-    </div>
-  )
-}
+    <div className="menu-container">
+      <div className="menu">
+        <h1>Welcome to the React Quiz</h1>
 
-export default MainMenu
+        <div class="container">
+          <span class="react-logo"></span>
+        </div>
+
+        <h1>Press Start to begin the quiz</h1>
+        <button
+          onClick={() => setGameState("quiz")}
+          className="btn-hover color-1"
+        >
+          Start
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default MainMenu;
